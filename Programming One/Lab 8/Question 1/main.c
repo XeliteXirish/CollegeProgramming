52a5c304a0ba7fdd326f96c709967490387dc948
XeliteXirish <shaunoneill7@me.com>
2017-03-20 14:20:13 +0000

    Some lab 8

--- Programming One/Lab 8/Question 1/main.c
+++ Programming One/Lab 8/Question 1/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
+    int x;
     char name[256];
+    srand(time(NULL));
 
     printf("Please enter your name: ");
+    scanf("%s", &name);
 
-    scanf("%s", &name)
+    for (x = 0; x <= rand() % 20; x++){
+        printf("Output %d: %s\n", x, name);
+    }
+
 
     return 0;
 }
