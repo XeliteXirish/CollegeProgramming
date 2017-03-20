3e9ba99d22f1dbe0833e9db7c4bb9facaa63c8a0
XeliteXirish <shaunoneill7@me.com>
2017-03-20 14:12:22 +0000

    Lab 8

---
+++ Programming One/Lab 8/Question 1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[256];
+
+    printf("Please enter your name: ");
+
+    scanf("%s", &name)
+
+    return 0;
+}
---
+++ Programming One/Lab 8/Question 2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    double random[10];
+    srand(time(NULL));
+
+    for(x = 0; x < 10; x++){
+        random[x] = 1 + (rand() & 1000);
+    }
+
+    printf("Array output:\n\n");
+    for(x = 0; x < 10; x++){
+            printf("Number %d: %.2lf\n", x, random[x]);
+    }
+
+    return 0;
+}
