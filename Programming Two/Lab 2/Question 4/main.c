03a19f558ea9100b5c1f74b0805c460f265e31bd
XeliteXirish <shaunoneill7@me.com>
2017-09-29 10:00:46 +0100

    Question 4

--- Programming Two/Lab 2/Question 4/main.c
+++ Programming Two/Lab 2/Question 4/main.c
@@ -4,7 +4,9 @@
 
 int main()
 {
-    double x1, y1, x2, y2, x3, y3, perm;
+    double returnLength(double x1, double y1, double x2, double y2);
+
+    double x1, y1, x2, y2, x3, y3, len1, len2, len3, perm;
 
     printf("Please enter coord 1 (format x;y): ");
     scanf("%lf;%lf", &x1, &y1);
@@ -15,12 +17,18 @@ int main()
     printf("\nPlease enter coord 3 (format x;y): ");
     scanf("%lf;%lf", &x3, &y3);
 
+    len1 = returnLength(x1, y1, x2, y2);
+    len2 = returnLength(x2, y2, x3, y3);
+    len3 = returnLength(x3, y3, x1, y1);
 
-    perm = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    perm = len1 + len2 + len3;
 
     printf("\nTriangle perimeter: %.2lf", perm);
 
-
-
     return 0;
+}
+
+double returnLength(double x1, double y1, double x2, double y2) {
+
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
 }
