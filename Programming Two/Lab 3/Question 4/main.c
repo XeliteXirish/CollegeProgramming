1467f07497028bce49ac1845e076f60c8a3afbcb
XeliteXirish <shaunoneill7@me.com>
2017-10-12 13:54:21 +0100

    Lab 4

--- Programming Two/Lab 3/Question 4/main.c
+++ Programming Two/Lab 3/Question 4/main.c
@@ -3,12 +3,29 @@
 
 float compute_GPA(float grades[], int n);
 
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+
+    int x, n = 12;
+    float grade, gpas[] = {}, computed = 0;
+
+    srand(time(NULL));
+
+    printf("Grades: ");
+    for (x = 0; x < n; x++) {
+        grade = rand() % 100;
+        gpas[x] = grade;
+        printf(" %f, ", &grade);
+    }
+
+    printf("\nThe average is: %f", compute_GPA(gpas, n));
+
     return 0;
 }
 
-flaot compute_GPA(float grades[], int n){
-
+float compute_GPA(float grades[], int n){
+    int x, average;
+    for (x = 0; x < n; x++) {
+        average += grades[x];
+    }
+    return average;
 }
--- Programming Two/Lab 4/Question 1/main.c
+++ Programming Two/Lab 4/Question 1/main.c
@@ -5,16 +5,18 @@ float largest(float *a, float *b);
 
 int main()
 {
-    float *num1, *num2, bigger = 0;
+    float *a, *b, num1, num2, bigger = 0;
+    a = &num1;
+    b = &num2;
 
-    while (bigger != &num1 || bigger != &num2) {
+    while (bigger != num1 || bigger != num2) {
         printf("\nPlease enter number one: ");
-        scanf("%f", num1);
+        scanf("%f", &num1);
 
         printf("\n\nPlease enter number two: ");
-        scanf("%f", num2);
+        scanf("%f", &num2);
 
-        bigger = largest(num1, num2);
+        bigger = largest(a, b);
     }
 
     printf("\nThe biggest number is: %f", bigger);
@@ -23,10 +25,10 @@ int main()
 }
 
 float largest(float *a, float *b){
-    if (&a > &b) return &a;
-    else if (&b > &a) return &a;
-    else if (&a == &b) {
+    if (*a > *b) return *a;
+    else if (*b > *a) return *a;
+    else if (*a == *b) {
         printf("\nSorry but you can't enter the same number twice, try again!");
-        return &a + 10
-    };
+        return *a + 10;
+    }
 }
