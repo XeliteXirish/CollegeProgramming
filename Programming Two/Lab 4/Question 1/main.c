7af74b5780acdf2af4ff9aa6dfaee5916bc6ccec
XeliteXirish <shaunoneill7@me.com>
2017-10-12 10:32:33 +0100

    Lab 4

---
+++ Programming Two/Lab 4/Question 1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float largest(float *a, float *b);
+
+int main()
+{
+    float *num1, *num2, bigger = 0;
+
+    while (bigger != &num1 || bigger != &num2) {
+        printf("\nPlease enter number one: ");
+        scanf("%f", num1);
+
+        printf("\n\nPlease enter number two: ");
+        scanf("%f", num2);
+
+        bigger = largest(num1, num2);
+    }
+
+    printf("\nThe biggest number is: %f", bigger);
+
+    return 0;
+}
+
+float largest(float *a, float *b){
+    if (&a > &b) return &a;
+    else if (&b > &a) return &a;
+    else if (&a == &b) {
+        printf("\nSorry but you can't enter the same number twice, try again!");
+        return &a + 10
+    };
+}
