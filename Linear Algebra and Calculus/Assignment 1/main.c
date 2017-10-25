38af33d9fcddd1aeb83f3363a4bdbc9289eb63db
XeliteXirish <shaunoneill7@me.com>
2017-10-25 11:02:39 +0100

    Finishing touches

--- Linear Algebra and Calculus/Assignment 1/main.c
+++ Linear Algebra and Calculus/Assignment 1/main.c
@@ -35,7 +35,25 @@ int main()
     printf("Enter the number of Columns: ");
     scanf("%d", &c2);
 
-    while (c1 != r2)
+    while (c1 > 10 || c2 > 10 || r1 > 10 || r2 > 10)   // Makes sure numbers are less then or equal to 10
+    {
+        printf("\nError! Columns and rows cannot be greater then 10! Try again!\n");
+
+        printf("\n\n---Matrix A---\n");
+        printf("Enter Number of Rows:");
+        scanf("%d",&r1);
+        printf("Enter the number of Columns: ");
+        scanf("%d", &c1);
+
+
+        printf("\n\n---Matrix B---\n");
+        printf("Enter Number of Rows: ");
+        scanf("%d",&r2);
+        printf("Enter the number of Columns: ");
+        scanf("%d", &c2);
+    }
+
+    while (c1 != r2) // Makes sure columns are equal to rows
     {
         printf("Error! Column of the Matrix A is not equal to Row of Matrix B\nPlease enter the rows and columns for both matrices again");
 
@@ -58,7 +76,8 @@ int main()
 
 //getting the values for first matrix
     printf("\n-MATRIX A-\n");
-    for(x=0; x<r1; x++){
+    for(x=0; x<r1; x++)
+    {
         for(y=0; y<c1; y++)
         {
             fscanf(input,"%d",&matrix1[x][y]);
@@ -69,7 +88,8 @@ int main()
 
 //Getting the values for second matrix
     printf("\n-MATRIX B-\n");
-    for (x = 12; x < (r2 + 12); x++) {
+    for (x = 12; x < (r2 + 12); x++)
+    {
         for(y=0; y<c2; y++)
         {
             fscanf(input, "%d", &matrix2[x-12][y]);
