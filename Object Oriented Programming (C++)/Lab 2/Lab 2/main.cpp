53bb201074268eed921c08e36b2d75be720623f9
XeliteXirish <shaunoneill7@me.com>
2018-02-07 11:53:19 +0000

    Lab 2

--- Object Oriented Programming (C++)/Lab 2/Lab 2/main.cpp
+++ Object Oriented Programming (C++)/Lab 2/Lab 2/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <limits>
+#include <math.h>
 
 using namespace std;
 
@@ -15,72 +18,67 @@ vector<double> amountEuros;
 
 int main()
 {
-    int x;
-    double amount, exchangedTo, amountOfValues = 0, largest = 0, smallest = 9999999, sum = 0;
+    double amount, exchangedTo, largest = 0, smallest = 9999999, sum = 0;
     string currency;
 
-    while(cin >> amount >> currency) {
-        if (currency == "exit") return 0;
-        if (cin.fail() || amount == 0) {
-            cout << "Please enter a valid amount and currency!\n";
+    while(currency != "exit") // Keep looping
+    {
+        cin >> amount >> currency; // Accept inputs
+        if (currency == "exit") break;
+        cin.clear(); // Clear the input buffer
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        if (cin.fail()) // If the input failed, error and go back to the start of the loop
+        {
+            cout << "Please enter a valid amount and currency!" << endl;
             continue;
         }
 
         exchangedTo = to_euro(amount, currency);
-        if (exchangedTo == 0) {
-            cout << "Please enter a valid currency!\n";
-            break;
+        if (isnan(exchangedTo))
+        {
+            cout << "Please enter a valid currency!" << endl;
+            continue;
         }
 
-        sum += amount;
-        amountOfValues++;
+        sum += to_euro(amount, currency);
+        amountEuros.push_back(exchangedTo);
 
         cout << exchangedTo << " euro";
-        if (exchangedTo > largest) {
+        if (exchangedTo > largest)
+        {
             largest = exchangedTo;
             cout << " - New largest value " << largest << endl;
-        }else if (exchangedTo < smallest) {
+        }
+        else if (exchangedTo < smallest)
+        {
             smallest = exchangedTo;
             cout << " - New smallest value " << smallest << endl;
-        }else cout << "\n";
+        }
+        else cout << "\n";
     }
 
-    if (sum > 0) {
-
+    if (sum > 0)
+    {
         cout << "\nLowest Value: " << smallest << " - Largest Value: " << largest;
-        cout << "\nNumber of values entered: " << amountOfValues;
+        cout << "\nNumber of values entered: " << amountEuros.size();
         cout << "\nSum in euro: " << sum;
         cout << "\nSum in dollars: " << from_euro(sum, "dollars");
         cout << "\nSum in yen: " << from_euro(sum, "yen");
         cout << "\nSum in sterling: " << from_euro(sum, "sterling");
         cout << "\nSum in yuan: " << from_euro(sum, "yuan") << endl;
+
+        cout << "\nValues (in euro): ";
+        sort(amountEuros.begin(), amountEuros.end());
+        for (int x = 0; x < amountEuros.size(); x++)
+        {
+            cout << amountEuros[x] << ", ";
+        }
     }
 
     return 0;
 }
 
 double to_euro(double amount, string currency)
-{
-    if (currency == "dollars")
-    {
-        return amount * dollar;
-    }
-    else if (currency == "yen")
-    {
-        return amount * yen;
-    }
-    else if (currency == "sterling")
-    {
-        return amount * sterling;
-    }
-    else if (currency == "yuan")
-    {
-        return amount * yuan;
-    }
-    else return 0;
-}
-
-double from_euro(double amount, string currency)
 {
     if (currency == "dollars")
     {
@@ -98,6 +96,27 @@ double from_euro(double amount, string currency)
     {
         return amount / yuan;
     }
-    else return 0;
+    else return nan("");
+}
+
+double from_euro(double amount, string currency)
+{
+    if (currency == "dollars")
+    {
+        return amount * dollar;
+    }
+    else if (currency == "yen")
+    {
+        return amount * yen;
+    }
+    else if (currency == "sterling")
+    {
+        return amount * sterling;
+    }
+    else if (currency == "yuan")
+    {
+        return amount * yuan;
+    }
+    else return nan("");
 }
 
