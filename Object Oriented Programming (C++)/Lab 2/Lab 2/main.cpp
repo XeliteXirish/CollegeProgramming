462858767119daf698d00aadf4631c5b8d56b1e5
XeliteXirish <shaunoneill7@me.com>
2018-02-07 09:48:58 +0000

    Lab 2

--- Object Oriented Programming (C++)/Lab 2/Lab 2/main.cpp
+++ Object Oriented Programming (C++)/Lab 2/Lab 2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,26 +11,30 @@ const double yuan = 7.83;
 double to_euro(double, string);
 double from_euro(double, string);
 
+vector<double> amountEuros;
+
 int main()
 {
     int x;
-    double amount, exchangedTo, exchangedFrom, largest = 0, smallest = 9999999, sum = 0;
+    double amount, exchangedTo, amountOfValues = 0, largest = 0, smallest = 9999999, sum = 0;
     string currency;
 
-    while(currency != "exit") {
+    while(cin >> amount >> currency) {
         if (currency == "exit") return 0;
-        cin >> amount >> currency;
-
-        if (amount == 0) {
-            cout << "Please enter an amount greater then 0!";
+        if (cin.fail() || amount == 0) {
+            cout << "Please enter a valid amount and currency!\n";
             continue;
         }
+
         exchangedTo = to_euro(amount, currency);
         if (exchangedTo == 0) {
-            cout << "Please enter a valid currency!";
-            continue;
+            cout << "Please enter a valid currency!\n";
+            break;
         }
-        sum += exchangedTo;
+
+        sum += amount;
+        amountOfValues++;
+
         cout << exchangedTo << " euro";
         if (exchangedTo > largest) {
             largest = exchangedTo;
@@ -38,12 +43,17 @@ int main()
             smallest = exchangedTo;
             cout << " - New smallest value " << smallest << endl;
         }else cout << "\n";
+    }
 
+    if (sum > 0) {
+
+        cout << "\nLowest Value: " << smallest << " - Largest Value: " << largest;
+        cout << "\nNumber of values entered: " << amountOfValues;
         cout << "\nSum in euro: " << sum;
         cout << "\nSum in dollars: " << from_euro(sum, "dollars");
         cout << "\nSum in yen: " << from_euro(sum, "yen");
         cout << "\nSum in sterling: " << from_euro(sum, "sterling");
-        cout << "\nSum in yuan: " << from_euro(sum, "yuan");
+        cout << "\nSum in yuan: " << from_euro(sum, "yuan") << endl;
     }
 
     return 0;
