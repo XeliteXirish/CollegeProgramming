1855de3aa45cc67a8254348c88da32a19d902b3f
Shaun O'Neill <shaunoneill7@me.com>
2018-02-12 01:43:58 +0000

    Design and blur css

--- Web Tech/Assignment 1/js/sidebar.js
+++ Web Tech/Assignment 1/js/sidebar.js
@@ -1,4 +1,8 @@
+let headerString = 'Welcome to the super awesome Object Orientated Programming tutorial website';
+let headerStringLength = headerString.length;
 let navOpen = true;
+
+let counter = 0;
 
 function toggleNav() {
     console.log(`Nav currently open: ${navOpen}`)
@@ -11,4 +15,16 @@ function toggleNav() {
         document.getElementById("mainContent").style.marginLeft = "240px";
         navOpen = true;
     }
+}
+
+/*$(document).ready(function () {
+    setInterval(animateHeader, 100);
+});*/
+
+function animateHeader() {
+    if (counter === headerStringLength) counter = 0;
+
+    $('#titleText').text(headerString.substring(0, headerStringLength - counter));
+    console.log(headerString);
+    counter++;
 }
