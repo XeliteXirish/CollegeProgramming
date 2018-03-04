e2d90e54fa52e2460fc0353225867108a7c7f8a4
Shaun O'Neill <shaunoneill7@me.com>
2018-03-04 21:24:03 +0000

    Finish maths assignment

--- Statistics and Probability/Assignment 1/Assignment 1 - Part2/main.c
+++ Statistics and Probability/Assignment 1/Assignment 1 - Part2/main.c
@@ -8,38 +8,37 @@ double calMedian(double values[], int amount);
 double calLowerQuart(double values[], int amount);
 double calHigherQuart(double values[], int amount);
 
-double set1[] = {15, 20, 25, 30, 35, 40};
-double set2[] = {};
-double set3[] = {};
+double set1[] = {7, 12, 22, 51, 123, 277, 668, 273, 118, 52, 24, 12};
+double set2[] = {10, 16, 36, 84, 191, 482, 200, 79, 35, 16, 9, 7};
+double set3[] = {6, 9, 16, 34, 70, 166, 368, 140, 55, 26, 14, 10};
 
 int main()
 {
-    int amountValues = 6;
-    double standardD, mean;
+    int amountValSet = 12, x;
 
     // Set 1
-    printf("Set 1:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set1, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set1, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set1, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set1, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set1, amountValues));
+    printf("Method A:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set1, amountValSet));
+    printf("\n\tMean: %.2lfmm", calMean(set1, amountValSet));
+    printf("\n\tMedian: %.2lfmm", calMedian(set1, amountValSet));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set1, amountValSet));
+    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set1, amountValSet));
 
     // Set 2
-    printf("\n\nSet 2:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set2, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set2, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set2, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValues));
+    printf("\n\nMethod B:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set2, amountValSet));
+    printf("\n\tMean: %.2lfmm", calMean(set2, amountValSet));
+    printf("\n\tMedian: %.2lfmm", calMedian(set2, amountValSet));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValSet));
+    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValSet));
 
     // Set 3
-    printf("\n\nSet 3:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set3, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set3, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set3, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValues));
+    printf("\n\nMethod C:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set3, amountValSet));
+    printf("\n\tMean: %.2lfmm", calMean(set3, amountValSet));
+    printf("\n\tMedian: %.2lfmm", calMedian(set3, amountValSet));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValSet));
+    printf("\n\tUpper Quart: %.2lfmm\n\n", calHigherQuart(set2, amountValSet));
 
     return 0;
 }
--- Statistics and Probability/Assignment 1/Assignment 1/main.c
+++ Statistics and Probability/Assignment 1/Assignment 1/main.c
@@ -8,44 +8,40 @@ double calMedian(double values[], int amount);
 double calLowerQuart(double values[], int amount);
 double calHigherQuart(double values[], int amount);
 
-double set1[] = {15, 20, 25, 30, 35, 40};
-double set2[] = {};
-double set3[] = {};
+double set1[] = {256.18, 261.83, 256.61, 255.25, 250.75, 251.18, 245.63, 250.74, 251.02, 252.26, 249.62,
+252.67, 258.26, 252.70, 257.90, 262.36, 256.97, 254.54, 256.52, 251.24, 246.58, 244.34, 248.91, 245.28};
+double set2[] = {252.70, 257.90, 262.36, 256.97, 254.54, 256.52, 251.24, 246.58, 244.34, 248.91, 245.28,
+239.72, 245.07, 249.12, 254.81, 251.48, 257.16, 253.60, 249.20, 252.82, 251.66, 249.48, 245.38, 242.31, 237.15};
+double set3[] = {258.47, 252.91, 258.11, 262.57, 257.18, 254.75, 256.73, 251.45, 246.79, 244.55, 249.12, 245.49,
+ 239.93, 245.28, 249.33, 255.02, 251.69, 257.37, 253.81, 249.41, 253.03, 251.87, 249.69, 245.59, 242.52, 237.36};
 
 int main()
 {
-    int amountValues = 6, x;
-    double standardD, mean;
-
-    File *inputSet1;
-    inputSet1 = fopen("set1.txt", "r");
-    for (x = 0; x < 20; x++) {
-
-    }
+    int amountValSet1 = 24, amountValSet2 = 25, amountValSet3 = 26, x;
 
     // Set 1
-    printf("Set 1:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set1, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set1, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set1, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set1, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set1, amountValues));
+    printf("Method A:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set1, amountValSet1));
+    printf("\n\tMean: %.2lfmm", calMean(set1, amountValSet1));
+    printf("\n\tMedian: %.2lfmm", calMedian(set1, amountValSet1));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set1, amountValSet1));
+    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set1, amountValSet1));
 
     // Set 2
-    printf("\n\nSet 2:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set2, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set2, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set2, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValues));
+    printf("\n\nMethod B:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set2, amountValSet2));
+    printf("\n\tMean: %.2lfmm", calMean(set2, amountValSet2));
+    printf("\n\tMedian: %.2lfmm", calMedian(set2, amountValSet2));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValSet2));
+    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValSet2));
 
     // Set 3
-    printf("\n\nSet 3:");
-    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set3, amountValues));
-    printf("\n\tMean: %.2lfmm", calMean(set3, amountValues));
-    printf("\n\tMedian: %.2lfmm", calMedian(set3, amountValues));
-    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValues));
-    printf("\n\tUpper Quart: %.2lfmm", calHigherQuart(set2, amountValues));
+    printf("\n\nMethod C:");
+    printf("\n\tStandard Deviation: %.2lfmm", calStandardD(set3, amountValSet3));
+    printf("\n\tMean: %.2lfmm", calMean(set3, amountValSet3));
+    printf("\n\tMedian: %.2lfmm", calMedian(set3, amountValSet3));
+    printf("\n\tLower Quart: %.2lfmm", calLowerQuart(set2, amountValSet3));
+    printf("\n\tUpper Quart: %.2lfmm\n\n", calHigherQuart(set2, amountValSet3));
 
     return 0;
 }
@@ -76,12 +72,14 @@ double calStandardD(double values[], int amount)
     return sqrt(standardDeviation/amount);
 }
 
-double calLowerQuart(double values[], int amount) {
+double calLowerQuart(double values[], int amount)
+{
     int pos = amount / 4; // Uses an int so it rounds to the nearest position
     return values[pos];
 }
 
-double calHigherQuart(double values[], int amount) {
+double calHigherQuart(double values[], int amount)
+{
     int pos = (3 * amount) / 4; // Uses an int so it rounds to the nearest position
     return values[pos];
 }
