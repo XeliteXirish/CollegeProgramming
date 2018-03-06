b7608e4d42f7c7e7db56ad4163faa776eff7836d
XeliteXirish <shaunoneill7@me.com>
2018-03-06 11:57:28 +0000

    Shitty unmarked lab

--- Object Oriented Programming (C++)/Lab 5/Part 1/main.cpp
+++ Object Oriented Programming (C++)/Lab 5/Part 1/main.cpp
@@ -3,8 +3,7 @@
 
 using namespace std;
 void readWords();
-
-vector<WordCount> mywords;
+void countWords();
 
 class WordCount
 {
@@ -24,19 +23,45 @@ public:
     }
 };
 
+vector<WordCount> mywords;
+vector<string> words;
+
 int main()
 {
-
     readWords();
+    countWords();
     return 0;
 }
 
 void readWords()
 {
-    string words;
-    while (getline(cin, words) && words.length() )
-    {
-        mywords.push_back(newWord);
+    string temp;
+    cout << "Enter names separated by space characters. Enter q when finished.\n";
+    while (cin >> temp && temp != "q") {
+            words.push_back(temp) ;
     }
     cout << "Finished" << endl;
+}
+
+void countWords() {
+    // Fetch initial word to test against
+    for(vector<string>::const_iterator i = words.begin(); i != words.end(); ++i) {
+        string tempWord = *i;
+        int wordCount = 0;
+
+        // Count all of words in vectr
+        for(vector<string>::const_iterator i = words.begin(); i != words.end(); ++i) {
+            if (tempWord == *i) wordCount++;
+        }
+
+        WordCount newWord = WordCount(tempWord, wordCount);
+        mywords.push_back(newWord);
+        newWord.printf();
+    }
+}
+
+void outputValues() {
+    for(vector<WordCount>::const_iterator i = mywords.begin(); i != mywords.end(); ++i) {
+
+        }
 }
