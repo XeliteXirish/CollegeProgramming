76aa669a52eb2f40995e7c884d2c5d21d428900a
XeliteXirish <shaunoneill7@gmail.com>
2019-10-21 10:34:02 +0100

    Question 1

--- Ubiquitous Computing/Lab 2/Question1/app/src/main/java/com/shaunoneill/labone_a/MainActivity.java
+++ Ubiquitous Computing/Lab 2/Question1/app/src/main/java/com/shaunoneill/labone_a/MainActivity.java
@@ -1,21 +1,23 @@
 package com.shaunoneill.labone_a;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String INTENT_NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText editTextName = findViewById(R.id.editTextName);
+        final EditText editTextName = findViewById(R.id.editTextName);
         EditText editTextPassword = findViewById(R.id.editTextPassword);
         EditText editTextPhone = findViewById(R.id.editTextPhone);
         EditText editTextEmail = findViewById(R.id.editTextEmail);
@@ -24,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Thank you, your request is being processed", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SendingActivity.class);
+                intent.putExtra(INTENT_NAME, editTextName.getText().toString());
+                startActivity(intent);
             }
         });
     }
--- Ubiquitous Computing/Lab 2/Question1/app/src/main/java/com/shaunoneill/labone_a/SendingActivity.java
+++ Ubiquitous Computing/Lab 2/Question1/app/src/main/java/com/shaunoneill/labone_a/SendingActivity.java
@@ -1,6 +1,8 @@
 package com.shaunoneill.labone_a;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,12 @@ public class SendingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = getIntent();
+        String name = intent.getExtras().getString(MainActivity.INTENT_NAME);
+
         setContentView(R.layout.sending_activity);
+        TextView textViewName = findViewById(R.id.textViewName);
+        textViewName.setText("Thank you " + name + ", your request is being processed");
+
     }
 }
